4872f86d4489ee96a6c0de2f81c8489b755e8dde
Mojitogod <timzilinzi@gmail.com>
2022-07-24 17:17:40 +0200

    This is new comit

---
+++ src/containers/App.js
@@ -0,0 +1,40 @@
+import React, {Component} from "react";
+import CardList from "../components/CardList";
+import { robots } from "../robots";
+import SearchBox from "../components/SearchBox";
+import "./App.css";
+import Scroll from "../components/Scroll"
+
+class App extends Component  {
+    constructor(){
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ""
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})   
+    }
+
+    render(){
+        const {robots, searchfield} = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+        })
+        return (
+            <div className = "tc">
+                <h1 className="f2">RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <Scroll>
+                    <CardList robots={filteredRobots}/>
+                </Scroll>
+
+
+            </div>
+        );
+  }
+}
+
+export default App;
---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: "Brko",
+        username: "Brkič",
+        email: "brko.brkič@gmail.com"
+    },
+    {
+        id: 2,
+        name: "Mrko",
+        username: "Mrkič",
+        email: "mrko.mrkič@gmail.com"
+    },
+    {
+        id: 3,
+        name: "Vrko",
+        username: "Vrkič",
+        email: "vrko.vrkič@gmail.com"
+    },
+    {
+        id: 4,
+        name: "Arko",
+        username: "Arkič",
+        email: "arko.arkič@gmail.com"
+    },
+    {
+        id: 5,
+        name: "Crko",
+        username: "Crkič",
+        email: "crko.Crkič@gmail.com"
+    },
+    {
+        id: 6,
+        name: "Erko",
+        username: "Erkič",
+        email: "erko.erkič@gmail.com"
+    },
+    {
+        id: 7,
+        name: "Frko",
+        username: "Frkič",
+        email: "frko.frkič@gmail.com"
+    },
+    {
+        id: 8,
+        name: "Grko",
+        username: "Grkič",
+        email: "grko.grkič@gmail.com"
+    },
+    {
+        id: 9,
+        name: "Hrko",
+        username: "Hrkič",
+        email: "hrko.hrkič@gmail.com"
+    }, {
+        id: 10,
+        name: "Irko",
+        username: "Irkič",
+        email: "irko.irkič@gmail.com"
+    },
+    
+]
